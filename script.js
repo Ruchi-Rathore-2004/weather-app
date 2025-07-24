f94b5b62b9337e24a995a2cb2ae9301bad7df1d0
Ruchi-Rathore-2004 <167629369+Ruchi-Rathore-2004@users.noreply.github.com>
2025-07-24 11:46:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// let modeBtn = document.querySelector("#mode");
+// let body = document.querySelector("body");
+// let currMode = "light";
+
+// modeBtn.addEventListener("click", ()=>{
+//     if(currMode === "light"){
+//         currMode = "dark"
+// //   document.querySelector("body").style.backgroundColor = "black";
+//      body.classList.add("dark");
+//     } else {
+//         currMode = "light";
+//         // document.querySelector("body").style.backgroundColor = "white";
+//         body.classList.add("light");
+//     }
+//     console.log(currMode);
+// });
+
+// let modeBtn = document.querySelector("#mode");
+// let body = document.querySelector("body");
+// let currMode = "light";
+
+// modeBtn.addEventListener("click", () => {
+//     if (currMode === "light") {
+//         currMode = "dark";
+//         body.classList.remove("light"); // Remove light mode
+//         body.classList.add("dark");    // Add dark mode
+//     } else {
+//         currMode = "light";
+//         body.classList.remove("dark"); // Remove dark mode
+//         body.classList.add("light");   // Add light mode
+//     }
+//     console.log(currMode);
+// }); 
+
+
+setTimeout(()=>{
+    console.log("hello")
+},2000);
